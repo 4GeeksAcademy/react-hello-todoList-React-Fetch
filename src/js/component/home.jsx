ee39b24750b437294c33b28525dedf6915d40bb7
Orianac0698 <150297944+Orianac0698@users.noreply.github.com>
2024-04-12 22:38:55 +0000

    message

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [newTodo, setNewTodo] = useState("");
+	const [list, setList] = useState([])
+	
+
+	useEffect(() => {
+		obtenerDatosLista()
+		
+	}, [])
+
+	const obtenerDatosLista = async () => {
+		try{
+			const respDatosUsuario = await fetch("https://playground.4geeks.com/todo/users/Oriana")
+			const data2 = await respDatosUsuario.json()
+			setList(data2.todos)
+			
+		}
+		catch (error){
+			console.error("Datos incompletos", error)
+
+		}
+		
+	}
+
+	const AddTodo = async () => {
+		try{
+			const respDatosUsuario = await fetch("https://playground.4geeks.com/todo/todos/Oriana", 
+				{
+					method:"POST",
+					headers:{
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"label": newTodo,
+						"is_done": false
+					  })
+				});
+
+			const data2 = await respDatosUsuario.json()
+			setList(data2.todos)
+			
+		}
+		catch (error){
+			console.error("Tarea incompleta", error)
+
+		}
+		
+	}
+
+	const  deleteTask = async () => {
+		try{
+			const respDatosUsuario = await fetch("https://playground.4geeks.com/todo/todos/Oriana", 
+				{
+					method:"DELETE",
+					headers:{
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"label": newTodo,
+						"is_done": false
+					  })
+				});
+
+			const data2 = await respDatosUsuario.json()
+			setList(data2.todos)
+			
+		}
+		catch (error){
+			console.error("no se pudo eliminar tarea", error)
+
+		}
+		
+	}
+
+//Logica
+
+	const handleClick = () => {
+		console.log("New task", newTodo);
+		AddTodo()
+		obtenerDatosLista()
+		setList([...list, newTodo]);
+	}
+
+
+	const deleteTodo = (index) => {
+
+		const NewListTodo = list.filter((list, i) => i !== index);
+		setList(NewListTodo);
+	}
+
+	const handleChange = (e) => {
+		setNewTodo(e.target.value);
+
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center ">
+
+			<h1 className="text-center mt-5"> My TodoList </h1>
+
+			<div className="container ">
+
+
+				<div className="d-flex gap-2">
+					<input type="text" className="form-control" onChange={handleChange} value={newTodo} />
+
+					<button onClick={handleClick} className="btn btn-outline-primary" >
+						Agregar tarea
+					</button>
+				</div>
+
+
+				<p> New task: {newTodo}</p>
+
+				<ul className="list-group d-flex justify-content-between">
+
+
+
+					 {list.map((item, index) => {
+						return (
+							<li key={index} className={`list-group-item active d-flex justify-content-between alaing-items-center border border-dark ${index % 2 === 0 ? "bg-primary" : ""}`}>
+								{item.label}
+
+								<button className="btn-close btn-close-white " onClick={() => deleteTodo(index)}/>
+							</li>
+						)
+					})} 
+				</ul>
+
+			</div>
+
 		</div>
 	);
 };
